c70f298b532ab084fe9d414b488aa5a2d357b57a
leofraga93 <49664647+leofraga93@users.noreply.github.com>
2026-05-20 23:23:59 -0300

    Ajuste de responsividade do projeto.

--- React/crud/frontend/cadastro-usuario/src/components/user/UserCrud.jsx
+++ React/crud/frontend/cadastro-usuario/src/components/user/UserCrud.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, use } from "react";
 import Main from '../template/Main'
 import axios from "axios";
 
@@ -18,6 +18,12 @@ export default class UserCrud extends Component {
 
   state = {...initialState}
 
+  componentDidMount(){
+    axios(baseUrl).then(resp => {
+      this.setState({list: resp.data})
+    })
+  }
+
   clear(){
     this.setState({user: initialState.user})
   }
@@ -33,9 +39,9 @@ export default class UserCrud extends Component {
       })
   }
 
-  getUpdatedList(user){
+  getUpdatedList(user, add = true){
     const list = this.state.list.filter(u => u.id !== user.id)
-    list.unshift(user)
+    if(add) list.unshift(user)
     return list
   }
 
@@ -88,11 +94,63 @@ export default class UserCrud extends Component {
     )
   }
 
-  render(){
+  load(user){
+    this.setState({user})
+  }
+
+  remove(user){
+    axios.delete(`${baseUrl}/${user.id}`).then(resp => {
+      const list = this.getUpdatedList(user, false)
+      this.setState({list})
+    })
+  }
+
+  renderTable(){
+    return (
+      <table className="table mt-4">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Nome</th>
+            <th>E-mail</th>
+            <th>Ações</th>
+          </tr>
+        </thead>
+        <tbody>
+          {this.renderTableRows()}
+        </tbody>
+      </table>
+    )
+  }
+
+  renderTableRows(){
+    return this.state.list.map(user => {
+      return (
+        <tr key={user.id}>
+          <td>{user.id}</td>
+          <td>{user.name}</td>
+          <td>{user.email}</td>
+          <td>
+            <button className="btn btn-warning"
+            onClick={()=> this.load(user)}>
+              <i className="fa fa-pencil"></i>
+            </button>
+            <button className="btn btn-danger ml-2"
+            onClick={()=> this.remove(user)}>
+              <i className="fa fa-trash"></i>
+            </button>
+          </td>
+        </tr>
+      )
+    })
+  }
+
+  render(){  
     return (
       <Main {...headerProps}>
         {/* Cadastro de Usuário */}
         {this.renderForm()}
+        {this.renderTable()}
       </Main>
     )
   }
